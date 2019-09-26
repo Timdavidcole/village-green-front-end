5a22ad64d965ff3a9058aeb6ff26c432854a4c64
timdavidcole <timdavidcole@gmail.com>
2019-09-26 12:50:59 +0100

    cleaned up project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { createStore } from 'redux';
 
@@ -9,28 +8,14 @@ const reducer = function(state = defaultState, action) {
     case 'TOGGLE':
       return { ...state, checked: !state.checked };
     default:
-      return state
+      return state;
   }
 };
 const store = createStore(reducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
