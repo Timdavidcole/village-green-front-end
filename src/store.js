d49d265bddab7e51453a6099a30eaa76df27509c
timdavidcole <timdavidcole@gmail.com>
2019-09-26 16:04:23 +0100

    refactored out store from index

---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { applyMiddleware, createStore } from "redux";
+import { promiseMiddleware } from "./middleware";
+
+const defaultState = {
+  appName: "conduit",
+  notices: null
+};
+const reducer = function(state = defaultState, action) {
+  // eslint-disable-next-line default-case
+  switch (action.type) {
+    case "HOME_PAGE_LOADED":
+      return { ...state, notices: action.payload.notices };
+  }
+  return state;
+};
+
+const middleware = applyMiddleware(promiseMiddleware);
+
+const store = createStore(reducer, middleware);
+
+export default store;
