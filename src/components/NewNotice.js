5e639f947181c5945af1d85daf7b3d989b19e9e3
Timdavidcole <timdavidcole@gmail.com>
2019-11-21 23:43:44 +0000

    new notice component basic layout

--- src/components/NewNotice.js
+++ src/components/NewNotice.js
@@ -7,10 +7,12 @@ import { Link } from "react-router-dom";
 const mapStateToProps = state => ({ ...state.auth });
 
 const mapDispatchToProps = dispatch => ({
-  onChangeEmail: value =>
-    dispatch({ type: "UPDATE_FIELD_AUTH", key: "email", value }),
-  onChangePassword: value =>
-    dispatch({ type: "UPDATE_FIELD_AUTH", key: "password", value }),
+  onChangeTitle: value =>
+    dispatch({ type: "UPDATE_FIELD_AUTH", key: "title", value }),
+  onChangeDescription: value =>
+    dispatch({ type: "UPDATE_FIELD_AUTH", key: "description", value }),
+  onChangeBody: value =>
+    dispatch({ type: "UPDATE_FIELD_AUTH", key: "body", value }),
   onSubmit: (email, password) =>
     dispatch({ type: "LOGIN", payload: agent.Auth.login(email, password) }),
   onUnload: () => dispatch({ type: "LOGIN_PAGE_UNLOADED" })
@@ -19,9 +21,10 @@ const mapDispatchToProps = dispatch => ({
 class NewNotice extends React.Component {
   constructor() {
     super();
-    this.changeEmail = event => this.props.onChangeEmail(event.target.value);
-    this.changePassword = event =>
-      this.props.onChangePassword(event.target.value);
+    this.changeTitle = event => this.props.onChangeTitle(event.target.value);
+    this.changeDescription = event =>
+      this.props.onChangeDescription(event.target.value);
+    this.changeBody = event => this.props.onChangeBody(event.target.value);
     this.submitForm = (email, password) => event => {
       event.preventDefault();
       this.props.onSubmit(email, password);
@@ -33,7 +36,7 @@ class NewNotice extends React.Component {
   }
 
   render() {
-    const notice = this.props.notice;
+    const { title, description, body } = this.props;
 
     return (
       <div
@@ -45,9 +48,49 @@ class NewNotice extends React.Component {
           padding: "5px"
         }}
       >
-          <h3>Enter title here...</h3>
-          <h5>Enter description here...</h5>
-          <span>Enter body here...</span>
+        <h3>Post a Notice</h3>
+        <form onSubmit={this.submitForm(title, description, body)}>
+          <fieldset>
+            <fieldset className="form-group">
+              <input
+                className="form-control form-control-lg"
+                type="title"
+                placeholder="Enter title here..."
+                value={title}
+                onChange={this.changeTitle}
+              />
+            </fieldset>
+
+            <fieldset className="form-group">
+              <input
+                className="form-control form-control-lg"
+                type="description"
+                placeholder="Enter description here..."
+                value={description}
+                onChange={this.changeDescription}
+              />
+            </fieldset>
+
+            <fieldset className="form-group">
+              <input
+                className="form-control form-control-lg"
+                type="body"
+                placeholder="Enter body here..."
+                value={body}
+                onChange={this.changeBody}
+              />
+            </fieldset>
+
+            <button
+              className="btn btn-lg btn-primary pull-xs-right"
+              type="submit"
+              disabled={this.props.inProgress}
+            >
+              {" "}
+              Post Notice
+            </button>
+          </fieldset>
+        </form>
       </div>
     );
   }
