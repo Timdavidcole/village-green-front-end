c4525ae66d8b81ba1ab6809534c7ca75400b3039
Timdavidcole <timdavidcole@gmail.com>
2020-03-20 00:14:04 +0000

    Added sort by page tests

--- src/models/sortByPage.js
+++ src/models/sortByPage.js
@@ -1,35 +1,36 @@
 import sumHeights from "./sumHeights";
 
-const sortByPage = function(noticesInColumns, noticesDims, noticeWidth) {
+const sortByPage = function(noticesInColumns, noticesWindowDims, noticeWidth) {
   let sortedByPage = [];
   let freePage = 0;
-  const maxHeight = noticesDims.height - 90;
-  const maxColumns = Math.floor(noticesDims.width / noticeWidth);
-  console.log(noticesInColumns)
+  const maxHeight = noticesWindowDims.height - 90;
+  const maxColumns = Math.floor(noticesWindowDims.width / noticeWidth);
+
   noticesInColumns.forEach(noticeColumn => {
     if (!sortedByPage[freePage]) {
-      console.log('push new array')
       sortedByPage.push([]);
     }
     if (sumHeights(noticeColumn) > maxHeight / 3) {
       if (sortedByPage[freePage].length < maxColumns) {
-        console.log(sortedByPage)
-        console.log(noticeColumn)
         sortedByPage[freePage].push(noticeColumn);
-        console.log(sortedByPage)
       } else {
         sortedByPage.push([]);
         freePage++;
         sortedByPage[freePage].push(noticeColumn);
-        console.log(sortedByPage)
       }
     }
   });
 
-  console.log(sortedByPage)
+  function flatten(array) {
+    return array.reduce(function(flat, toFlatten) {
+      return flat.concat(
+        Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten
+      );
+    }, []);
+  }
+
   sortedByPage = sortedByPage.map(page => {
-    console.log(page)
-    return page.flat();
+    return flatten(page);
   });
 
   return sortedByPage;
