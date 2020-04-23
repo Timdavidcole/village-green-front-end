1a3062f574267e7c8384a7a22acb45a91ea12fe1
Timdavidcole <timdavidcole@gmail.com>
2020-04-23 22:35:59 +0100

    Set up new register page

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,38 +2,28 @@ import React from "react";
 import ListErrors from "./ListErrors";
 import agent from "../agent";
 import { connect } from "react-redux";
-import AddressContainer from "./AddressContainer";
+import "../styles/register.css"
 
-const mapStateToProps = state => ({ ...state.auth });
+const mapStateToProps = (state) => ({ ...state.auth });
 
-const mapDispatchToProps = dispatch => ({
-  onChangeEmail: value =>
-    dispatch({ type: "UPDATE_FIELD_AUTH", key: "email", value }),
-  onChangePassword: value =>
-    dispatch({ type: "UPDATE_FIELD_AUTH", key: "password", value }),
-  onChangeUsername: value =>
-    dispatch({ type: "UPDATE_FIELD_AUTH", key: "username", value }),
-  onChangeAddress: value =>
-    dispatch({ type: "UPDATE_FIELD_AUTH", key: "address", value }),
+const mapDispatchToProps = (dispatch) => ({
   onSubmit: (username, email, password, address) =>
     dispatch({
       type: "REGISTER",
-      payload: agent.Auth.register(username, email, password, address)
-    })
+      payload: agent.Auth.register(username, email, password, address),
+    }),
 });
 
 class Register extends React.Component {
   constructor() {
     super();
-    this.changeEmail = event => this.props.onChangeEmail(event.target.value);
-    this.changePassword = event =>
-      this.props.onChangePassword(event.target.value);
-    this.changeUsername = event =>
-      this.props.onChangeUsername(event.target.value);
-    this.changeAddress = event => {
-      this.props.onChangeAddress(event.target.value);
+    this.state = {
+      email: "",
+      password: "",
+      username: "",
+      address: "",
     };
-    this.submitForm = (username, email, password, address) => event => {
+    this.submitForm = (username, email, password, address) => (event) => {
       event.preventDefault();
       this.props.onSubmit(username, email, password, address);
     };
@@ -41,8 +31,7 @@ class Register extends React.Component {
   }
 
   addressAutoComplete() {
-
-    if (this.props.address && this.props.addressAutoComplete) {
+    if (this.state.address && this.props.addressAutoComplete) {
       return (
         this.props.addressAutoComplete.address.houseNumber +
         " " +
@@ -58,80 +47,71 @@ class Register extends React.Component {
   }
 
   render() {
-    const { email, password, username, address } = this.props;
+    const { email, password, username, address } = this.state;
 
     return (
-      <div className="auth-page">
-        <div className="container page">
-          <div className="row">
-            <div className="col-md-6 offset-md-3 col-xs-12">
-              <h1 className="text-xs-center">Sign up</h1>
-              <ListErrors errors={this.props.errors} />
-              <form
-                onSubmit={this.submitForm(
-                  username,
-                  email,
-                  password,
-                  this.addressAutoComplete()
-                )}
-              >
-                <fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="email"
-                      placeholder="Email"
-                      value={email}
-                      onChange={this.changeEmail}
-                    />
-                  </fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="password"
-                      placeholder="Password"
-                      value={password}
-                      onChange={this.changePassword}
-                    />
-                  </fieldset>
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="username"
-                      placeholder="Username"
-                      value={username}
-                      onChange={this.changeUsername}
-                    />
-                  </fieldset>
-                  Address
-                  <fieldset className="form-group">
-                    <input
-                      className="form-control form-control-lg"
-                      type="text"
-                      value={address}
-                      onChange={this.changeAddress}
-                    />
-                  </fieldset>
-                  <AddressContainer />
-                  <button
-                    className="btn btn-lg btn-primary pull-xs-right"
-                    type="submit"
-                    disabled={this.props.inProgress}
-                  >
-                    {" "}
-                    Sign Up
-                  </button>
-                </fieldset>
-              </form>
-            </div>
-          </div>
-        </div>
+      <div>
+        <ListErrors errors={this.props.errors} />
+        <form
+          className="new-notice-form"
+          onSubmit={this.submitForm(
+            username,
+            email,
+            password,
+            this.addressAutoComplete()
+          )}
+        >
+          <fieldset className="new-notice-fieldset">
+            <input
+              className="register-input"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(ev) => {
+                this.setState({ email: ev.target.value });
+              }}
+            />
+            <input
+              className="register-input"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(ev) => {
+                this.setState({ password: ev.target.value });
+              }}
+            />
+            <input
+              className="register-input"
+              type="username"
+              placeholder="Username"
+              value={username}
+              onChange={(ev) => {
+                this.setState({ username: ev.target.value });
+              }}
+            />
+            Address
+            <input
+              className="register-input"
+              type="text"
+              value={address}
+              placeholder={this.addressAutoComplete}
+              onChange={(ev) => {
+                this.setState({ address: ev.target.value });
+              }}
+            />
+            <button
+              className="register-button"
+              type="submit"
+              disabled={this.props.inProgress}
+            >
+              {" "}
+              Sign Up
+            </button>
+          </fieldset>
+        </form>
       </div>
     );
   }
 }
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Register);
+export default connect(mapStateToProps, mapDispatchToProps)(Register);
